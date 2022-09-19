e30fb29375ca7ad079d98690521e227c48433121
jaikishan <jaikishan.purohit@v2solutions.com>
2022-09-19 19:20:21 +0530

    age restriction problem completed

--- jsassignment2/agerestriction.js
+++ jsassignment2/agerestriction.js
@@ -1,12 +1,22 @@
-/* var employee = {
+var employee = {
     name : "Coder",
     role : "Trainee",
-    age : 68,
+    age : 25,
     location : "Bangalore",
 };
-employee.age = 25;
-*/
-/* Write a code to sort this array */
+Object.defineProperty(employee, 'age',{
+
+    set: function (age) {
+        if (age < 65 && age > 17) {
+            age = age;
+        } else {
+            throw 'Invalid age';
+        }
+    }
+});
+employee.age = 26;
+
+/* Write a code to sort this array */ 
 var a = [4,8,15,16,23,12];
 sorted = a.sort(function (a,b) {return a-b });
-console.log(sorted);   
+console.log(sorted);
